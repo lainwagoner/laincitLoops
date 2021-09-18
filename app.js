848fac0950c79c253fcb614c1595ba3dd7532987
lainwagoner <89354036+lainwagoner@users.noreply.github.com>
2021-09-18 19:41:52 -0400

    Resubmission

--- app.js
+++ app.js
@@ -1,59 +1,33 @@
-let writerEle = document.body.querySelector(".box");
 
-// The array
-let animals = ["dog", "fish", "cat", "shark", "cat", "dog"]
+//apply button and array info
+function jumble(shuffleArray = false) {
+    let writerstring = "";
+    let animals = ["dog", "fish", "cat", "shark", "cat", "dog"];
+    let box = document.getElementsByClassName("box")[0];
 
-// Code for if/else statements
-for(let i = 0; i <animals.length; i++) {
-    let ele = document.createElement("div");
+    if (shuffleArray) shuffle(animals);
 
+    for (let i = 0; i < animals.length; i++) {
     if (animals[i] === "dog") {
-        ele.innerHTML = "borf borf";
-        writerEle.appendChild(ele);
-
+   writerstring += "borf borf <br/>";
     } else if (animals[i] === "cat") {
-        ele.innerHTML = "I am a cat";
-        writerEle.appendChild(ele);
-
+        writerstring += "I am a cat <br/>";
     } else {
-        ele.innerHTML = "I am an animal";
-        // ele.innerHTML=animals[i];
-        writerEle.appendChild(ele);
+       writerstring += "I am an animal <br/>";
+        }
+    }
+    //array shuffling
+box.innerHTML = writerstring;
+    function shuffle(array) {
+        for (let i = array.length - 1; i >0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    }
 
     }
-    writerEle.appendChild(ele);
-}
-
-// Code for loop
-function jumble() {
-
-    for (let i = animals.length - 1; i > 0; i--) {
-       const j = Math.floor(Math.random() * (i + 1));
-        let temp = animals[i];
-        animals[i] = animals[j];
-        animals[j] = temp;
-    }
-}
-jumble(animals);
-
-/*code for button #first way (how do I connect the function to the button???)
-let btn = document.createElement("button");
-    btn.innerHTML = "Click Me";
-    btn.addEventListener("click",function jumble (){ //why wont it run the function?
-    });
-
-    document.body.appendChild(btn);*/
+jumble();
 
 
-// Code for button #second way AGAIN (how do I connect the function to the button???)
-let button = document.createElement("button");
-button.innerHTML = "Click Me"
-
-let body = document.getElementsByTagName("body")[0];
-body.appendChild(button);
-
-button.addEventListener ("click", function() {
-    jumble();
-});
 
 
